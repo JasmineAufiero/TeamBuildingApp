bb5b013008255df7aa9ffc5c53b28cf1c46593ff
JasmineAufiero <jasmineaufiero@gmail.com>
2022-06-22 09:50:49 +0200

    Initial Commit

---
+++ TeamBuildingProject/TeamBuildingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamBuildingProjectApp.swift
+//  TeamBuildingProject
+//
+//  Created by Jasmine Aufiero on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TeamBuildingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
