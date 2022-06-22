d23e2b47f356a8942120397ec1fb630baaf4a075
JasmineAufiero <jasmineaufiero@gmail.com>
2022-06-22 12:53:31 +0200

    Added image and name of the place inside Detail View

--- TeamBuildingProject/Views/PlacesDetail.swift
+++ TeamBuildingProject/Views/PlacesDetail.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct PlacesDetail: View {
+    var place :Place
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 10) {
+            Image(place.name)
+                .resizable()
+                .frame(width: 170, height: 170, alignment: .leading)
+                .clipShape(Circle())
+            
+            Text(place.name)
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding()
+        }
     }
 }
 
 struct PlacesDetail_Previews: PreviewProvider {
     static var previews: some View {
-        PlacesDetail()
+        PlacesDetail(place: Place(name: "Panificio Michelangelo Bobb", suggestions: "Pizza" , description: "You can order take-away food directly from the Academy and collect it a maximum of one hour later. Bobb is a bakery that produces baked goods of the typical Neapolitan cuisine. Here you can find bread, obviously pizza, but also the lesser known by non-Neapolitans, taralli, Neapolitan panini, casatielli (rustic pies with ricotta and salami). Warning: most Neapolitan food contains cured meats. The portions are very generous!", category: "food", location: "Viale Michelangelo, 27, 80040 Cercola NA", link: "https://apps.apple.com/it/app/bobb/id1538023603", isFavorite: false))
     }
 }
